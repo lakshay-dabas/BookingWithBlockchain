39f9845ebe53db988be7a134954151f3e129f2df
Vipin Yadav <vipinyadav1041998@gmail.com>
2020-06-17 11:19:52 +0530

    doing

--- app.js
+++ app.js
@@ -1,41 +1,31 @@
+const express  =require('express');
+// const exphbs = require('express-handlebars');
+const mongoose = require('mongoose')
+const path = require('path')
 require('dotenv').config();
-const Web3 = require('web3'); //API for client to connect to Ethereum blockchain network
-const Tx = require('ethereumjs-tx'); //module for creating, manipulating and signing transaction
 
-const PRIVATE_KEY_1 = Buffer.from(process.env.PRIVATE_KEY_1,'hex');//convert in hex form
+const app = express();//init
+const router = express.Router();
 
-const account1 = '0x6D1B479aB3E2b95884bA2499179e958E13ce3610';
-const account2 = '0x692991888659c3e8Ad043B262B0AF97415eA4aDB';
+// app.engine('handlebars', exphbs());//creating new template engine 
+// app.set('view engine', 'handlebars');//setting view engine to handlbars engine (respnsible for file with .handlebars ending)
 
-const web3 = new Web3('https://ropsten.infura.io/v3/96beb9df66384d07991430e7fa44a4a2'); //web3 is a instance for connection to robsten network
+mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser : true,useUnifiedTopology: true})
+    .then(() => console.log('Mongo DB connected'))
+    .catch((err) => console.log(err));
 
-web3.eth.getBalance(account1, function(error, result) {
-    console.log('balance if account1', web3.utils.fromWei(result,'ether'));
-});
 
-web3.eth.getBalance(account2, function(error, result) {
-    console.log('balance if account2', web3.utils.fromWei(result,'ether'));
-});
 
-web3.eth.getTransactionCount(account1)
-    .then(txCount => {
-        console.log(txCount);
-        const txObject = {
-            nonce : web3.utils.toHex(txCount),
-            to : account2,
-            value : web3.utils.toHex(web3.utils.toWei('0.1','ether')),
-            gasLimit : web3.utils.toHex(21000),
-            gasPrice : web3.utils.toHex(web3.utils.toWei('10','gwei'))
-        }
-        const tx = new Tx(txObject);
-        tx.sign(PRIVATE_KEY_1);
+const {transer} = require('./helperFunc');
 
-        const serializedTransaction = tx.serialize();//converting object to bytes form, so that it   can be saved in memory/db and 
-        //by serialized form of object we can re instantiate object easily 
-        const raw = '0x' + serializedTransaction.toString('hex');
-        // console.log(raw);
-        web3.eth.sendSignedTransaction(raw)
-            .then(info => console.log(info))
-            .catch(err => console.log(err));
-    })
-    .catch(err => console.log(err));
+
+//we are using this to parse form data
+app.use(express.urlencoded( {extended : true}));
+app.use(express.json());  //middleware for json payload parsing
+
+
+app.use(express.static(path.join(__dirname ,'public')));
+const PORT  = process.env.PORT | 3000;
+app.listen(PORT, () => {
+    console.log('Listening at port ' + PORT);
+})
---
+++ helperFunc.js
@@ -0,0 +1,48 @@
+
+const Web3 = require('web3'); //API for client to connect to Ethereum blockchain network
+const Tx = require('ethereumjs-tx'); //module for creating, manipulating and signing transaction
+
+
+function transfer(account1,account2){
+    const PRIVATE_KEY_1 = Buffer.from(process.env.PRIVATE_KEY_1,'hex');//convert in hex form
+
+    // const account1 = '0x6D1B479aB3E2b95884bA2499179e958E13ce3610';
+    // const account2 = '0x692991888659c3e8Ad043B262B0AF97415eA4aDB';
+//     PRIVATE_KEY_1 = 8e05ecf21d5c508b6886c17685954989bff3f0fd9b51c03407af5d425fe0f25d
+// PRIVATE_KEY_2 = ffa7524a274e71b2cac23dad02c3fda9332f44435465da16ff4f74dbcd81aff3
+
+    const web3 = new Web3('https://ropsten.infura.io/v3/96beb9df66384d07991430e7fa44a4a2'); //web3 is a instance for connection to robsten network
+
+    web3.eth.getBalance(account1, function(error, result) {
+        console.log('balance if account1', web3.utils.fromWei(result,'ether'));
+    });
+
+    web3.eth.getBalance(account2, function(error, result) {
+        console.log('balance if account2', web3.utils.fromWei(result,'ether'));
+    });
+
+    web3.eth.getTransactionCount(account1)
+        .then(txCount => {
+            console.log(txCount);
+            const txObject = {
+                nonce : web3.utils.toHex(txCount),
+                to : account2,
+                value : web3.utils.toHex(web3.utils.toWei('0.1','ether')),
+                gasLimit : web3.utils.toHex(21000),
+                gasPrice : web3.utils.toHex(web3.utils.toWei('10','gwei'))
+            }
+            const tx = new Tx(txObject);
+            tx.sign(PRIVATE_KEY_1);
+
+            const serializedTransaction = tx.serialize();//converting object to bytes form, so that it   can be saved in memory/db and 
+            //by serialized form of object we can re instantiate object easily 
+            const raw = '0x' + serializedTransaction.toString('hex');
+            // console.log(raw);
+            web3.eth.sendSignedTransaction(raw)
+                .then(info => console.log(info))
+                .catch(err => console.log(err));
+        })
+        .catch(err => console.log(err));
+}
+
+module.exports = {transfer};
